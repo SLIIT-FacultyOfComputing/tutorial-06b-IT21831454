53d5ebc48769aa442e31c6bff75a9adece2cc0c1
Pasindu Peiris <110684306+IT21831454@users.noreply.github.com>
2022-09-23 06:01:26 +0000

    Did tutorial

--- Student.cpp
+++ Student.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 
 // Assign studentId and name
-Student::assignDetails(int Id, char n[]) {
+void Student::assignDetails(int Id, char n[]) {
 studentId = Id;
 strcpy(name,n);
   
@@ -11,7 +11,7 @@ strcpy(name,n);
 }
 
 // Display StudentId and Name
-Student::display() {
+void Student::display() {
 
   cout<<"Student Id : "<<studentId<<endl;
   cout<<"student name : "<<name;
